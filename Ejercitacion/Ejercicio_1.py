b1361f82904edd3601682ec08727cc36970c3d01
Ignacio Chaves <ignaciochaves@pop-os.localdomain>
2023-03-27 16:02:46 -0300

    ejer1 update

--- Ejercitacion/Ejercicio_1.py
+++ Ejercitacion/Ejercicio_1.py
@@ -1,17 +1,18 @@
-#! /usr/bin/python
+#!/usr/bin/python3
 
 import sys
 
-def oddNumbersGenerator(n = sys.argv[1]):
+def oddNumbersGenerator():
     try:
-        n = int(n)
-        oddNumList = []
-        while len(oddNumList) < n:
-              oddNumList.append(len(oddNumList)+1)
+        n = int(sys.stdin[1])
+        oddNumList = [counter for counter in range(1, n*2, 2)]
         print(oddNumList)
         # sys.stdout.write(', '.join(oddNumList))
     except:
-        sys.stdout.write("The argument must be a whole and positive integer")
+        sys.stdout.write("This method requires an argument, witch must be a whole and positive integer")
 
 if __name__ == "__main__":
     oddNumbersGenerator()
+
+
+#agregar libreria argparce y getopt
