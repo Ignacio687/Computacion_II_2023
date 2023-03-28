abdc0758fa1202edbc65744bce850fca5eb4425d
Ignacio687 <ignadavidchaves@gmail.com>
2023-03-28 13:39:03 -0300

    ejercicio 1 y 2 working

---
+++ Ejercitacion/ejercicio_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+
+def oddNumbersGenerator():
+    try:
+        n = int(sys.argv[1])
+        oddNumList = [counter for counter in range(1, n*2, 2)]
+        sys.stdout.write(str(oddNumList))
+    except:
+        sys.stdout.write("This method requires an argument, which must be positive integer")
+
+if __name__ == "__main__":
+    oddNumbersGenerator()
+
+
+#implementar libreria argparce y getopt
