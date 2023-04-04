c5959869d3d6a44ea63a3568276755eb76b75b0f
Ignacio687 <ignadavidchaves@gmail.com>
2023-04-04 17:22:28 -0300

    ejercicio 4 done

--- Ejercitacion/ejercicio_4/ejercicio_4.py
+++ Ejercitacion/ejercicio_4/ejercicio_4.py
@@ -2,17 +2,17 @@
 
 import os 
 import sys
-import math
+import cmath
 import argparse
 
 class rootsCalculator:
 
     def __init__(self):
         self.parser = argparse.ArgumentParser(
-            program = 'rootsCalculator',
-            description='calculate positive and negative roots of a number',)
-        self.parser.add_argument('integer', metavar='n', type=int, help='the integer you want to know the square root of')
-        self.parser.add_argument('forkFlag',metavar='-f', action='store_true', help='if you want the child process to calculate the negative square root')
+            prog = 'rootsCalculator',
+            description='calculate positive and negative roots of a number')
+        self.parser.add_argument('n', type=int, help='the integer you want to know the square root of')
+        self.parser.add_argument('-f', '--forkFlag', action='store_true', help='if you want the child process to calculate the negative square root')
         self.args = self.parser.parse_args()
         self.PPID = os.getpid()
         self.CPID = 0
@@ -22,11 +22,11 @@ class rootsCalculator:
             os.fork()
         if self.PPID == os.getpid():
             print(f'Parent PID: {self.PPID}')
-            print(math.sqrt(self.args.integer))
+            print(cmath.sqrt(self.args.n))
         else:
             self.CPID = os.getpid()
             print(f'Child PID: {self.CPID}')
-            print(-math.sqrt(self.args.integer))
+            print(-cmath.sqrt(self.args.n))
 
 if __name__ == '__main__':
     app = rootsCalculator()
