eac6552f1d5772487d0f55b62d5ff26c042afde1
Ignacio687 <ignadavidchaves@gmail.com>
2023-04-16 04:20:44 -0300

    ejercicio1 clase 11-4 finished

--- Ejercitacion/clase_11-4/ejercicio_1.py
+++ Ejercitacion/clase_11-4/ejercicio_1.py
@@ -4,10 +4,11 @@
 Cada elemento deberá calcularse en un proceso distinto devolviendo el resultado 
 en una fifo indicando el indice del elemento. El padre deberá leer en la fifo y mostrar el resultado final.'''
 
-import os, time, sys, tempfile
+import os, time, sys
 
 class MultiProcessMatrixCalculator():
     def calculate(self, matrix1, matrix2):
+        os.mkfifo('fifo')
         pidFather = os.getpid()
         pid = os.fork()
         child = 0
@@ -16,30 +17,23 @@ class MultiProcessMatrixCalculator():
                 pid = os.fork()
                 child = forkCount
         if os.getpid() == pidFather:
-            fd, tempfilename = tempfile.mkstemp()
-            os.rename(tempfilename, 'fifo')
-            #os.mkfifo('fifo')
             while True:
-                time.sleep(1)
+                time.sleep(0.2)
                 with open('fifo', 'r') as fifo:
-                    results = fifo.readlines()   
+                    results = fifo.readlines()
                 if len(results) > 3:
                     sys.stdout.write(''.join(results))
                     os.remove('fifo')
                     break
         else:
-            if child < 2:
-                result = (matrix1[0][0]*matrix2[0][child])+(matrix1[0][1]*matrix2[1][child])
-            else:
-                result = (matrix1[1][0]*matrix2[0][child-2])+(matrix1[1][1]*matrix2[1][child-2])
-            while True:
-                if os.path.isfile('fifo'):
-                    with open('fifo', 'a') as fifo:
-                        if child < 2:
-                            fifo.write(f'0{child}:{result}\n')
-                        else:
-                            fifo.write(f'1{child-2}:{result}\n')
-                    break
+            row = 0 if child < 2 else 1
+            col = 0 if child == 0 or child == 2 else 1
+            result = 0
+            for rowAndColCount in range (0, len(matrix1[0])):
+                result += (matrix1[row][rowAndColCount] * matrix2[rowAndColCount][col])
+            with open('fifo', 'a') as fifo:
+                fifo.write(f'{row}{col}:{result}\n')
+
 
 if __name__=='__main__':
     matrixList = [
