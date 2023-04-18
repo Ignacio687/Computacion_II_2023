1d3d0ef903c6ee8a1506f828c20ecba63dbb5b80
Ignacio687 <ignadavidchaves@gmail.com>
2023-04-17 23:33:58 -0300

    ej1_clase_11-4 2.3

--- Ejercitacion/clase_11-4/ejercicio_1.py
+++ Ejercitacion/clase_11-4/ejercicio_1.py
@@ -4,37 +4,37 @@
 Cada elemento deberá calcularse en un proceso distinto devolviendo el resultado 
 en una fifo indicando el indice del elemento. El padre deberá leer en la fifo y mostrar el resultado final.'''
 
-import os, time, sys
+import os, sys
 
 class MultiProcessMatrixCalculator():
     def run(self, matrix1, matrix2):
         os.mkfifo('fifo')
         pid = 1
-        for forkCount in range(0, 5):
-            #Child Process calculations and fifo write
-            if pid == 0:
-                row = 0 if child < 2 else 1
-                col = 0 if child % 2 == 0 else 1
-                result = 0
-                for rowAndColCount in range (0, 2):
-                    result += (matrix1[row][rowAndColCount] * matrix2[rowAndColCount][col])
-                with open('fifo', 'a') as fifo:
-                    fifo.write(f'{row}{col}:{result}\n')
-                os._exit(0)
-            #Parent Process fifo read
-            elif forkCount == 4:
-                while True:
-                    time.sleep(0.2)
-                    with open('fifo', 'r') as fifo:
-                        results = fifo.readlines()
-                    if len(results) > 3:
-                        sys.stdout.write(''.join(results))
-                        os.unlink('fifo')
-                        break
-            #Parent Process child generation
-            else:
+        #Parent Process child generation
+        for forkCount in range(0, 4):
+            if pid != 0:
                 pid = os.fork()
                 child = forkCount
+        #Child Process calculations and fifo write
+        if pid == 0:
+            row = 0 if child < 2 else 1
+            col = 0 if child % 2 == 0 else 1
+            result = 0
+            for rowAndColCount in range (0, 2):
+                result += (matrix1[row][rowAndColCount] * matrix2[rowAndColCount][col])
+            with open('fifo', 'a') as fifo:
+                fifo.write(f'{row}{col}:{result}\n')
+            os._exit(0)
+        #Parent Process fifo read
+        results = []
+        for childInput in range (0, 4):
+            with open('fifo', 'r') as fifo:
+                results += fifo.readlines()
+            if len(results) == 4:
+                os.unlink('fifo')
+                sys.stdout.write(''.join(results))
+                break
+                
 
 
 
