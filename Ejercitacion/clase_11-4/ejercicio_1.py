002ee57b7938e007161f00e5a69c70b75237ebae
Ignacio687 <ignadavidchaves@gmail.com>
2023-04-17 22:21:32 -0300

    ej1_clase_11-4 2.1

--- Ejercitacion/clase_11-4/ejercicio_1.py
+++ Ejercitacion/clase_11-4/ejercicio_1.py
@@ -4,13 +4,25 @@
 Cada elemento deberá calcularse en un proceso distinto devolviendo el resultado 
 en una fifo indicando el indice del elemento. El padre deberá leer en la fifo y mostrar el resultado final.'''
 
-import os, time, sys, subprocess, unittest
+import os, time, sys
 
 class MultiProcessMatrixCalculator():
     def run(self, matrix1, matrix2):
         os.mkfifo('fifo')
+        pid = 1
         for forkCount in range(0, 5):
-            if forkCount == 4 and pid != 0:
+            #Child Process calculations and fifo write
+            if pid == 0:
+                row = 0 if child < 2 else 1
+                col = 0 if child % 2 == 0 else 1
+                result = 0
+                for rowAndColCount in range (0, 2):
+                    result += (matrix1[row][rowAndColCount] * matrix2[rowAndColCount][col])
+                with open('fifo', 'a') as fifo:
+                    fifo.write(f'{row}{col}:{result}\n')
+                os._exit(0)
+            #Parent Process fifo read
+            elif forkCount == 4:
                 while True:
                     time.sleep(0.2)
                     with open('fifo', 'r') as fifo:
@@ -19,19 +31,10 @@ class MultiProcessMatrixCalculator():
                         sys.stdout.write(''.join(results))
                         os.unlink('fifo')
                         break
-            elif forkCount == 0 or pid != 0:
-                subprocess.Popen('pstree > process.txt', shell=True)
+            #Parent Process child generation
+            else:
                 pid = os.fork()
                 child = forkCount
-            else:
-                row = 0 if child < 2 else 1
-                col = 0 if child == 0 or child == 2 else 1
-                result = 0
-                for rowAndColCount in range (0, len(matrix1[0])):
-                    result += (matrix1[row][rowAndColCount] * matrix2[rowAndColCount][col])
-                with open('fifo', 'a') as fifo:
-                    fifo.write(f'{row}{col}:{result}\n')
-                break
 
 
 
