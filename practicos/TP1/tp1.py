04abb7e1ac73fe728c63863529348f88e4d103ad
Ignacio687 <ignadavidchaves@gmail.com>
2023-05-02 05:23:55 -0300

    tp1 done

--- practicos/TP1/tp1.py
+++ practicos/TP1/tp1.py
@@ -1,25 +1,43 @@
-import os, argparse
+import os, argparse, sys
 
 class TextInverter():
     def __init__(self) -> None:
         parser = argparse.ArgumentParser(
-            prog = "Text Inverter", 
-            description = ("This program inverts the words and their"+
-            "order in every line found on the input text file"))
-        parser.add_argument("-f", "--file", action = "store", default = "/", 
-                            required = True, type = str, help = "Relative file path")
-        parser.add_argument("-a", "--absolute", action = "store_true", type = bool, 
-                            help = "Use this argument if you want to insert an absolute file path")
+            description = "This program inverts the words and their "+
+            "order in every line found on the input text file")
+        parser.add_argument("-f", "--file", action = "store", required = True, 
+                            type = str, help = "Relative file path")
         self.args = parser.parse_args()
 
     def run(self):
-        pass
+        try:
+            file = os.open(self.args.file, os.O_RDONLY)
+            lines = os.read(file, 2048).decode().split("\n")
+        except FileNotFoundError:
+            sys.stdout.write("The file doesn't exist\n")
+            sys.exit()
+        except IsADirectoryError:
+            sys.stdout.write("Must select a file not a directory\n")
+            sys.exit()
+        pipes = {}
+        for child in range(0, len(lines)):
+            if lines[child] != "":
+                pipes[child] = os.pipe()
+                os.write(pipes[child][1], lines[child].encode())
+                pid = os.fork()
+                if pid == 0:
+                    line = os.read(pipes[child][0], 2048).decode()
+                    os.close(pipes[child][0])
+                    line = line[::-1]
+                    os.write(pipes[child][1], line.encode())
+                    sys.exit()
+        try:        
+            while True:
+                os.wait()
+        except ChildProcessError:
+            for pipe in pipes.values():
+                sys.stdout.write(os.read(pipe[0], 2048).decode()+"\n")
 
-    def mksubp(self):
-        pass
-
-    def inverter(self, text):
-        pass
 
 if __name__ == "__main__":
     app = TextInverter()
